f010feafd3aa4a19fed7d6cf100ef9bf6b18983a
Nima Jafari <Nima.jafari.47@gmail.com>
2023-01-25 10:44:19 +0330

    Add Database

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=50, unique= True)
+    email = models.EmailField(max_length=50, unique = True)
+
+
+class EmotionText(models.Model):
+    emotion_name = models.CharField(max_length=50, unique=True)
+
+
+class DataText(models.Model):
+    text = models.TextField()
+    emotion_primary = models.ForeignKey(EmotionText, on_delete=models.DO_NOTHING, null=True, blank=True)
+    emotion_secondary = models.ManyToManyField(EmotionText, null=True, blank=True)
+    class Meta:
+        verbose_name_plural = "Data"
+
+
