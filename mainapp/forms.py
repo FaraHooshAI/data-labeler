6ac85352eec6a8d4d4383db94c8b673066dfa89b
Nima Jafari <Nima.jafari.47@gmail.com>
2023-01-28 15:01:42 +0330

    create form ok now

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,9 +1,22 @@
 from django import forms
-from .models import EmotionText, DataText
+from .models import EmotionText, DataText, DataName
 
+emotions = [(x.id, x) for x in EmotionText.objects.all()]
 class DataRow(forms.ModelForm):
     class Meta:
-        model = EmotionText
-        fields = ['data_name', 'text', 'emotion_primary', 'emotion_secondary']
+        model = DataText
+        fields = ['data_name', 'text']
+
+    emotions_primary = forms.ChoiceField(
+        choices= emotions,
+        widget=forms.RadioSelect()
+    )
+
+    emotions_secondary = forms.ModelMultipleChoiceField(
+        queryset= EmotionText.objects.all(),
+        widget=forms.CheckboxSelectMultiple()
+    )
+
+
 
     
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.edit import UpdateView, CreateView
+from django.views import View
+from .models import DataText, EmotionText
+from .forms import DataRow
 # farahoosh super user
 def index(request):
     return render(request, 'mainapp/index.html')
+
+class DataRow(CreateView):
+    model = DataText
+    form_class = DataRow
+    template_name = 'mainapp/data_row.html'
+    success_url = 'mainapp/index.html'
+    print([x for x in EmotionText.objects.all()])
+   
+
