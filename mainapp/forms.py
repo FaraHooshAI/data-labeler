ff7a3728cf16422154a36ba01010532d74cad209
Nima Jafari <Nima.jafari.47@gmail.com>
2023-01-28 16:39:06 +0330

    Radio and checkbox works correctly

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -2,21 +2,24 @@ from django import forms
 from .models import EmotionText, DataText, DataName
 
 emotions = [(x.id, x) for x in EmotionText.objects.all()]
-class DataRow(forms.ModelForm):
-    class Meta:
-        model = DataText
-        fields = ['data_name', 'text']
 
-    emotions_primary = forms.ChoiceField(
-        choices= emotions,
+class DataRow(forms.ModelForm):
+    emotion_primary = forms.ChoiceField(
+        choices=emotions,
         widget=forms.RadioSelect()
     )
-
-    emotions_secondary = forms.ModelMultipleChoiceField(
-        queryset= EmotionText.objects.all(),
+    emotion_secondary = forms.ModelMultipleChoiceField(
+        label='seconday emotions',
+        queryset=EmotionText.objects.all(),
         widget=forms.CheckboxSelectMultiple()
     )
-
+    class Meta:
+        model = DataText
+        exclude = ['deleted']
+        
+    # def __init__(self, *args, **kwargs):
+    #         super(DataRow, self).__init__(*args, **kwargs)
+    #         self.fields['emotion_primary'].widget = forms.RadioSelect
 
 
     
